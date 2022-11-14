c8813019f8fc34b31578c8b9fb38bb7ee8aded07
ah0y <nalfmalf@gmail.com>
2022-11-14 22:17:23 +0300

    handles varying length for program2

--- program2.exs
+++ program2.exs
@@ -4,23 +4,30 @@ defmodule M do
   end
 
   def input_to_list(input) do
-    input
-    |> String.split("\n\n")
-    |> Enum.map(fn list ->
-      list
-      |> String.split([", ", ",", " ", "\n", "[", "]"])
-      |> Enum.reject(&(&1 == ""))
-      |> Enum.map(&String.to_integer/1)
-    end)
+    lists =
+      input
+      |> String.split("\n\n")
+      |> Enum.map(fn list ->
+        list
+        |> String.split([", ", ",", " ", "\n", "[", "]"])
+        |> Enum.reject(&(&1 == ""))
+        |> Enum.map(&String.to_integer/1)
+      end)
+
+    max_length =
+      lists
+      |> Enum.map(&length/1)
+      |> Enum.max()
+
+    lists
+    |> Enum.map(fn list -> list ++ List.duplicate(0, max_length - length(list)) end)
   end
 
   def calculate(lists) do
     lists
     |> Enum.zip_with(fn [x, y, z] ->
       [x, y, z]
-      |> Enum.filter(&is_integer/1)
       |> Enum.filter(&(rem(&1, 2) == 0))
-      |> Enum.filter(&is_integer/1)
       |> Enum.max(fn -> 0 end)
     end)
     |> Enum.sum()
